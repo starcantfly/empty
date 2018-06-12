a43fb0d5bfe8891ea9aea2472cd80416be18d5ad
starcantfly <starcantfly@aliyun.com>
2018-06-12 11:26:28 +0800

    添加persion类

---
+++ src/empty/Persion.java
@@ -0,0 +1,20 @@
+package empty;
+
+public class Persion {
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
