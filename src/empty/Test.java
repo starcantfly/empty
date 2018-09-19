7c2c036a468b9e2e81975c9f88c6e2ef7b68f2e9
ex_huangchangxin <ex_huangchangxin@axatp.com>
2018-09-19 17:25:59 +0800

    sdsd

--- src/empty/Test.java
+++ src/empty/Test.java
@@ -3,18 +3,8 @@ package empty;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("helloworld chang branch test");
-		System.out.println("helloworld chang branch test");
-		System.out.println("helloworld chang branch test");
-		System.out.println("2222222222222");
-		System.out.println("helloworld chang branch test");
-		System.out.println("2222222222222");
-		System.out.println("helloworld chang branch test");
-		System.out.println("2222222222222");
-		System.out.println("helloworld chang branch test");
-		System.out.println("2222222222222");
-		System.out.println("2222222222222");
-		System.out.println("2222222222222");
+		String name="helloworld";
+		System.out.println(name.substring(name.length()-1,name.length()));
 	}
 
 }
